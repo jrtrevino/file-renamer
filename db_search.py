8ded41e852ab1932831b74a02a5084b0121dc9e5
Joey <jrtrevin@calpoly.edu>
2021-09-25 16:41:03 -0700

    Fixed results formatting

--- db_search.py
+++ db_search.py
@@ -1,25 +1,59 @@
 # used tmdbsimple wrapper created by celiao.
 # https://github.com/celiao/tmdbsimple/
-
-import tmdbsimple as tmdb
 import movie
+import secrets
+import sys
 
-tmdb.API_KEY = ''
+try:
+    import tmdbsimple as tmdb
+except ImportError as e:
+    print("Import Missing: {}".format(e))
+    sys.exit(-1)
+
+tmdb.API_KEY = secrets.get_api_key()
+
 
 def search_for_movie(mov_obj):
-    # create search_obj
     search = tmdb.Search()
+    counter = 0
+    new_title = None
     response = search.movie(query=mov_obj.get_title())
-    counter = 1
-
-    if int(response['total_results']) >= 1 and mov_obj.get_year() == -1:
-        print(response['total_results'], " total search results. Listing...")
+    # print(response)
+    # show all results because we are unsure of the movie's year from parsing
+    if response['total_results'] >= 1 and mov_obj.get_year() == -1:
+        print(response['total_results'],
+              " total search results. Listing Titles...")
         for results in search.results:
-            print("[" + str(counter) + "] ", results['title'], results['release_date'])
+            print("[{}]".format(counter),
+                  results['title'], results['release_date'])
             counter += 1
+        try:
+            movie_val = int(input(
+                '\nPlease enter the number of the movie to rename the file to. Or -1 to cancel.\n'))
+            if movie_val >= 0 and movie_val <= counter:
+                results = search.results[movie_val]
+                print("Renaming movie to: {} ({})".format(
+                    results['title'], results['release_date'][:4]))
+                new_title = "{} ({})".format(
+                    results['title'], results['release_date'][:4])
+            elif movie_val == -1:
+                print("Canceling.")
+            else:
+                print(
+                    "Sorry, please enter a valid number between 0 and {}".format(counter))
+        except Exception as e:
+            print(e)
+            print("Sorry, we couldn't rename the file using the API query. Try again.")
 
     elif int(response['total_results']) >= 1:
         for results in search.results:
             if str(mov_obj.get_year()) in results['release_date']:
-                print("Best match found: ", results['title'], results['release_date'])
+                print("Match Found. Title: {}, Year: {}".format(
+                    results['title'], results['release_date'][:4]))
+                new_title = "{} ({})".format(
+                    results['title'], results['release_date'][:4])
                 break
+    if new_title:
+        return new_title
+    else:
+        return None
