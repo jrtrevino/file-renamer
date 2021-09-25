9559bcff19dc1e7d011b5ed774a2e50728c69efc
Joey <jrtrevin@calpoly.edu>
2021-09-25 16:41:25 -0700

    Added search to API

--- movie_parse.py
+++ movie_parse.py
@@ -5,12 +5,16 @@ import db_search
 import re
 
 # top level function to begin parsing process.
+
+
 def begin_program():
     num_files = len(sys.argv)
     for id in range(1, num_files):
         new_name = parse_movie(sys.argv[id])
 
 # begins the process to parse movie information from a filename.
+
+
 def parse_movie(path):
     # check for file existance.
     if not (check_path_existance(path)):
@@ -24,6 +28,8 @@ def parse_movie(path):
     return mov_obj
 
 # returns a movie object containing information found from parsing.
+
+
 def create_movie_object(info_arr):
     temp_movie_obj = movie.Movie(info_arr["title"], info_arr["year"])
     temp_movie_obj.add_codec(info_arr["codec"])
@@ -32,6 +38,8 @@ def create_movie_object(info_arr):
     return temp_movie_obj
 
 # determines if current word in filename is considered a valid year.
+
+
 def find_year(dict, word):
     return_val = -1
     # remove possible parenthesis from year
@@ -46,54 +54,62 @@ def find_year(dict, word):
     return return_val
 
 # determines if current word in filename is a valid resolution.
+
+
 def find_quality(dict, word):
     if (word.lower() == "480p" or word.lower() == "720p" or
-     word.lower() == "1080p" or word.lower() == "2160p"):
+            word.lower() == "1080p" or word.lower() == "2160p"):
         dict["quality"] = word
         return 1
     return -1
 
 # determines if current word in filename is a valid video codec.
+
+
 def find_codec(dict, word):
     if (word.casefold() == "x264" or word.casefold() == "x265"
-    or "hevc" in word.casefold() or "xvid" in word.casefold()):
+            or "hevc" in word.casefold() or "xvid" in word.casefold()):
         dict["codec"] = word
         return 1
     return -1
 
 # attempts to determines the source of a video based off of keywords.
+
+
 def find_source(dict, word):
     if ("bluray" in word.casefold() or "dvdrip" in word.casefold()
-        or "hdtv" in word.casefold()):
+            or "hdtv" in word.casefold()):
         dict["source"] = word
         return 1
     return -1
 
 # attempts to find the movie title through parsing.
+
+
 def find_movie_info(file_array):
     title_found = False
     filename = []
     file_info = {
-        'title' : "",
-        'year' : -1,
-        'quality' : "",
-        'codec' : "",
-        'source' : ""}
+        'title': "",
+        'year': -1,
+        'quality': "",
+        'codec': "",
+        'source': ""}
     # cycle through filename and match informaiton.
     for element in file_array:
         # check if number in filename is the year
         if (find_year(file_info, element)) == 1:
-            title_found = True # big assumption...
+            title_found = True  # big assumption...
             continue
         elif find_quality(file_info, element) == 1:
-            title_found = True # big assumption...
+            title_found = True  # big assumption...
             continue
         elif find_codec(file_info, element) == 1:
-            title_found = True # big assumption...
+            title_found = True  # big assumption...
             continue
 
         elif find_source(file_info, element) == 1:
-            title_found = True # big assumption...
+            title_found = True  # big assumption...
             continue
 
         else:
@@ -105,7 +121,9 @@ def find_movie_info(file_array):
     return file_info
 
 # returns an array with elements consisting of the file information/
-def split_file (filename):
+
+
+def split_file(filename):
     # check for spaces or periods.
     split_name = filename.replace('.', " ")
     new_split = split_name.replace('-', " ")
@@ -113,32 +131,43 @@ def split_file (filename):
     return split_name
 
 # creates path for movie and renames old file.
+
+
 def rename_movie(path, movie):
     # find path directory and extension to properly name.
     extension = "." + os.path.splitext(path)[1][1:]
     dir = os.path.dirname(path) + "/"
     # we will ping themoviedb.org to get the movie title from parsed
     # info from the given file.
-    db_search.search_for_movie(movie)
-    final_name = dir + movie.search_string() + extension
+    api_name = db_search.search_for_movie(movie)
+    print("Result from api: {}".format(api_name))
+    if api_name:
+        final_name = dir + api_name + extension
+    else:
+        final_name = dir + movie.search_string() + extension
     os.rename(path, final_name)
 
 # checks if file exists. If not, next file (if any) begin parsing.
+
+
 def check_path_existance(path_to_file):
     if(os.path.exists(path_to_file)):
         return True
     else:
         (print("movie_parse: File does not exist: "
-        + str(path_to_file)))
+               + str(path_to_file)))
         return False
 
 # ensures the user inputted at least 1 file to be parsed.
+
+
 def check_arg():
     num_files = len(sys.argv)
     if num_files == 1:
         print("Usage: movie_parse.py <fileA> <fileB> ...")
         return 0
     return num_files
+
 
 if __name__ == '__main__':
     if (check_arg()):
